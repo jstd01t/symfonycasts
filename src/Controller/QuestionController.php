658347a238978764247bf512d60508f30a96826c
jstd01t <commontasksfor@gmail.com>
2022-04-26 17:36:30 +0300

    add custom repository methods for QueryBuilder, simplify controller with ParamConverter

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -66,17 +66,10 @@ EOF
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug, MarkdownHelper $markdownHelper, EntityManagerInterface $entityManager)
+    public function show(Question $question, MarkdownHelper $markdownHelper)
     {
         if ($this->isDebug) {
             $this->logger->info('We are in debug mode!');
-        }
-
-        $repository = $entityManager->getRepository(Question::class);
-        /** @var Question|null $question */
-        $question = $repository->findOneBy(['slug'=>$slug]);
-        if (!$question) {
-            throw $this->createNotFoundException(sprintf('no question found for slug "%s"', $slug));
         }
 
         $answers = [
