919dde542c2e13c7e1a28ff6607b136805b2d656
jstd01t <commontasksfor@gmail.com>
2022-04-28 18:15:37 +0300

    add: the votes property in the database, voting in the show page

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -5,10 +5,12 @@ namespace App\Controller;
 use App\Entity\Question;
 use App\Repository\QuestionRepository;
 use App\Service\MarkdownHelper;
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Sentry\State\HubInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
@@ -32,7 +34,7 @@ class QuestionController extends AbstractController
     public function homepage(QuestionRepository $repository)
     {
         $questions = $repository->findAllAskedOrderedByNewest();
-        return $this->render('question/homepage.html.twig',[
+        return $this->render('question/homepage.html.twig', [
             'questions' => $questions,
         ]);
     }
@@ -44,14 +46,15 @@ class QuestionController extends AbstractController
     {
         $question = new Question();
         $question->setName('Missing pants')
-            ->setSlug('missing-pants-'.rand(0, 1000))
+            ->setSlug('missing-pants-' . rand(0, 1000))
             ->setQuestion(<<<EOF
 Bla, `bla`, bla, bla?
 EOF
-);
-        if(rand(1,10)>2){
+            );
+        if (rand(1, 10) > 2) {
             $question->setAskedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
         }
+        $question->setVotes(rand(-20, 50));
 
         $entityManager->persist($question);
         $entityManager->flush();
@@ -66,19 +69,26 @@ EOF
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show(Question $question, MarkdownHelper $markdownHelper)
+    public function show(Question $question, MarkdownHelper $markdownHelper, EntityManagerInterface $entityManager)
     {
         if ($this->isDebug) {
             $this->logger->info('We are in debug mode!');
         }
+
+        // due to the @ParamConverter controller works without code bellow
+        //$repository = $entityManager->getRepository(Question::class);
+        ///** @var Question|null $question */
+        //$question = $repository->findOneBy(['slug'=>$slug]);
+        //if (!$question) {
+        //    throw $this->createNotFoundException(sprintf('no question found for slug "%s"', $slug));
+        //}
+
 
         $answers = [
             'Make sure your cat is sitting `purrrfectly` still ?',
             'Honestly, I like furry shoes better than MY cat',
             'Maybe... try saying the spell backwards?',
         ];
-
-
 
 
         return $this->render('question/show.html.twig', [
@@ -88,4 +98,23 @@ EOF
 
     }
 
+    /**
+     * @Route("/question/{slug}/vote", name="app_question_vote", methods="POST")
+     */
+    public function questionVote(Question $question, Request $request, EntityManagerInterface $entityManager)
+    {
+        $direction = $request->request->get('direction');
+
+        if ($direction === 'up') {
+            $question->upVote();
+        } elseif ($direction === 'down') {
+            $question->downVote();
+        }
+
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_question_show', [
+            'slug' => $question->getSlug(),
+        ]);
+    }
 }
